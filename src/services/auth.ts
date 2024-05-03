2407a632508a0fbc6523a934089194ad5717899d
Phuong <lephuong10142002@gmail.com>
2024-05-03 11:08:57 +0700

    update-interceptor-axios

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,13 +1,13 @@
-import axios from 'axios'
 //Config
 import { CONFIG_API } from 'src/configs/api'
+import instanceAxios from 'src/helpers/axios'
 
 //Type
 import { TLoginAuth } from 'src/types/auth'
 
 export const LoginAuth = async (data: TLoginAuth) => {
   try {
-    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/login`, data)
+    const res = await instanceAxios.post(`${CONFIG_API.AUTH.INDEX}/login`, data)
 
     return res.data
   } catch (error) {
@@ -17,7 +17,7 @@ export const LoginAuth = async (data: TLoginAuth) => {
 
 export const LogoutAuth = async () => {
   try {
-    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/logout`)
+    const res = await instanceAxios.post(`${CONFIG_API.AUTH.INDEX}/logout`)
 
     return res.data
   } catch (error) {
