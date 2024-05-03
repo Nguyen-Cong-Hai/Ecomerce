8c39daca8fa0f05c919f268496f6cd15e9d6c65b
Phuong <lephuong10142002@gmail.com>
2024-05-03 09:02:57 +0700

    update-auth-gest-guard

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -2,6 +2,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_HOST
 
 export const CONFIG_API = {
   AUTH: {
-    INDEX: `${BASE_URL}/auth/login`
+    INDEX: `${BASE_URL}/auth`,
+    AUTH_ME: `${BASE_URL}/auth/me`
   }
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -7,7 +7,17 @@ import { TLoginAuth } from 'src/types/auth'
 
 export const LoginAuth = async (data: TLoginAuth) => {
   try {
-    const res = await axios.post(CONFIG_API.AUTH.INDEX, data)
+    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/login`, data)
+
+    return res.data
+  } catch (error) {
+    return null
+  }
+}
+
+export const LogoutAuth = async () => {
+  try {
+    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/logout`)
 
     return res.data
   } catch (error) {
